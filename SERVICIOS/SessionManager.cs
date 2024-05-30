bacc9d592bfe00157f582071a9267bce11a287b9
Santiago Taylor <santiagotaylor03@gmail.com>
2024-05-30 18:20:08 -0300

    Diseño y programación de login (funcional)

---
+++ SERVICIOS/SessionManager.cs
@@ -0,0 +1,66 @@
+﻿using BE;
+using System;
+
+namespace SERVICES
+{
+    public class SessionManager
+    {
+        #region Atributos
+        private BE_Login _userLogin;
+        public BE_Login UserLogin { get => _userLogin; set => _userLogin = value; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        private static SessionManager _session;
+
+        public static SessionManager GetInstance
+        {
+            get
+            {
+                if (_session == null)
+                {
+                    throw new Exception("La sesión no está iniciada.");
+                }
+                return _session;
+            }
+        }
+
+        private static object _sessionLock = new object();
+        #endregion
+
+        #region Métodos
+        public static void Login(BE_Login login)
+        {
+            lock (_sessionLock)
+            {
+                if (_session == null)
+                {
+                    _session = new SessionManager();
+                    _session._userLogin = login;
+                    _session.StartDate = DateTime.Now;
+                }
+                else
+                {
+                    throw new Exception("La sesión ya está iniciada.");
+                }
+            }
+        }
+
+        public static void Logout()
+        {
+            lock (_sessionLock)
+            {
+                if (_session != null)
+                {
+                    _session = null;
+                }
+                else
+                {
+                    throw new Exception("La sesión no está iniciada.");
+                }
+            }
+        }
+        #endregion
+    }
+}
--- UI/Webforms/frmLogin.aspx.cs
+++ UI/Webforms/frmLogin.aspx.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using BLL;
+using System;
+using SERVICES;
 
 namespace UI.Webforms
 {
     public partial class frmLogin : System.Web.UI.Page
     {
+        private int _failedLogins = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (BLL_Login.IsValidCredentials(txtUser.Text, txtPassword.Text))
+            {
+                WebformMessage.ShowMessage("Bienvenido!", this);
+            }
+            else
+            {
+                WebformMessage.ShowMessage("El usuario o la contraseña son incorrectos", this);
+                _failedLogins++;
+            }
         }
     }
 }
