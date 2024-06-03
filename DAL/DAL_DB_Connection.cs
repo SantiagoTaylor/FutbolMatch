dc3f6d091332f62df3db1abe4892b41634236002
Christian Claros <95495732+Clarosabel2@users.noreply.github.com>
2024-06-02 22:29:56 -0300

    fix

---
+++ DAL/DAL_DB_Connection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    public class DAL_DB_Connection
+    {
+        private readonly MySqlConnection _connection = new MySqlConnection(@"Server=sql10.freesqldatabase.com;Database=sql10710948;User ID=sql10710948;Password=wasQWU7Y4l");
+
+        public MySqlConnection Connection => _connection;
+        public MySqlConnection OpenConnection()
+        {
+            if (Connection.State == ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+            return Connection;
+        }
+
+        public MySqlConnection CloseConnection()
+        {
+            if (Connection.State == ConnectionState.Open)
+            {
+                Connection.Close();
+            }
+            return Connection;
+        }
+    }
+}
