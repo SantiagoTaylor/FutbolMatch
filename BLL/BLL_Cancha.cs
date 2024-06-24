14636ec0c42fdb80318050dcb71096e6b659b031
brian-reser <brian.reser@outlook.com>
2024-06-24 12:00:22 -0300

    Se agrego la funcionalidad Realizar Reserva

---
+++ BLL/BLL_Cancha.cs
@@ -0,0 +1,25 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Caching;
+
+namespace BLL
+{
+    public class BLL_Cancha
+    {
+        private DAL_Cancha canchaDAL = new DAL_Cancha();
+
+        public List<BE.BE_Cancha> ObtenerCanchas()
+        {
+            return canchaDAL.ObtenerCanchas();
+        }
+
+        public void CrearCancha(BE.BE_Cancha cancha)
+        {
+            canchaDAL.CrearCancha(cancha);
+        }
+    }
+}
---
+++ DAL/DAL_Horario.cs
@@ -0,0 +1,61 @@
+﻿using BE;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_Horario
+    {
+        private string connectionString = Environment.GetEnvironmentVariable("MY_CONNECTION_STRING");
+
+        public List<BE_Horario> ObtenerHorarios(int canchaID)
+        {
+            List<BE_Horario> horarios = new List<BE_Horario>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT HorarioID, CanchaID, HoraInicio, HoraFin FROM tb_Horarios WHERE CanchaID = @CanchaID";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CanchaID", canchaID);
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            horarios.Add(new BE_Horario
+                            {
+                                HorarioID = reader.GetInt32("HorarioID"),
+                                CanchaID = reader.GetInt32("CanchaID"),
+                                HoraInicio = reader.GetTimeSpan("HoraInicio"),
+                                HoraFin = reader.GetTimeSpan("HoraFin")
+                            });
+                        }
+                    }
+                }
+            }
+
+            return horarios;
+        }
+
+        public void CrearHorario(BE_Horario horario)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO tb_Horarios (CanchaID, HoraInicio, HoraFin) VALUES (@CanchaID, @HoraInicio, @HoraFin)";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CanchaID", horario.CanchaID);
+                    cmd.Parameters.AddWithValue("@HoraInicio", horario.HoraInicio);
+                    cmd.Parameters.AddWithValue("@HoraFin", horario.HoraFin);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
